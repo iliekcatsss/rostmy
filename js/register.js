71c35c6baac730f589de921608efd076874b8b70
ILikeCats <iliekcatsss@proton.me>
2026-04-17 17:09:23 -0700

    feat: añadir página de registro

---
+++ js/register.js
@@ -0,0 +1,15 @@
+import { supabase } from "./supabase";
+
+document.querySelector('#btn-register').addEventListener('click', async () => {
+    const email = document.querySelector('#email').value
+    const pasword = document.querySelector('#password').value
+
+    const { error } = await supabase.auth.signUp({ email, password })
+
+    if (error) {
+        alert('Error: ' + error.message)
+    } else {
+        alert('Cuenta creada, ya puedes iniciar sesión')
+        window.location.href = '/login.html'
+    }
+})
